0effb6a84ca8a78f5ff63df0d68e10ac49e10e21
Andrew Core <andrew.core@computrols.com>
2019-04-10 14:14:00 -0500

    MySQLConnector: fixed add, moved Connection into the contructor, changed retrieve to execute a string. Parser: created a method to pull top ten words from the DB based on title
    MySQLConnector should be completed.

--- mySQLConnector.java
+++ mySQLConnector.java
@@ -1,52 +1,100 @@
 import java.sql.*;
 
-public class mySQLConnector 
+public class MySQLConnector 
 {
 	String username = null;
 	String password = null;
+	Connection con;
 	
-	public mySQLConnector(String user, String pass)
+	
+	public MySQLConnector(String user, String pass, String database)
 	{
 		username = user;
 		password = pass;
+		
+		try
+		{
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database, username, password);
+		}
+		catch(SQLException e)
+		{
+			System.out.println("Error: Unable to connect to DB");
+		}
+		
 	}
 	
-	public String retrieve(int index)
+	//con.close can throw an sql exception, but we don't really mind as closing isn't vital because of garbage collection
+	//This motivated me to make close it's own method, it serves to contain those exceptions to one spot.
+	public void close()
 	{
-		String doc;
-		
+		try { con.close(); }   //Sometimes terrible formatting is ok :p
+		catch (SQLException e) { System.out.println("Error: Couldn't close connection."); }
+	}
+	
+	
+	//Executes the passed statement, and returns a string.  The format of the string is as follows
+	//column 1 name|| column 2 name|| etc
+	//row1 column 1 data|| row1 column 2 data|| etc
+	//For as many rows and columns as you have.  
+	public String retrieve(String executeThis)
+	{
+		String doc = "";
 		try 
 		{
-			Class.forName("com.mysql.jdbc.Driver");  
+			//Class.forName("com.mysql.jdbc.Driver");  This is depriciated apparently  
+			//System.out.println("ok..."); 
 			
-
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", username, password);
 			Statement stmt = con.createStatement();  
-			ResultSet rs = stmt.executeQuery("show databases; ");  
-			for(int i = 0; rs.next() && i <= index; i++) {}
-				System.out.println((doc = rs.getString(2)));
-			con.close();  
+			ResultSet rs = stmt.executeQuery(executeThis);  
+			if(!rs.next())
+				return "";
+			
+			ResultSetMetaData metaData = rs.getMetaData();
+			int count = metaData.getColumnCount(), i;
+			
+			//This section records the results of the statement into doc.  Currently it also prints to the console;
+			if(count > 0)
+			{
+				//The first line is column names
+				for(i = 1; i < count; i++)
+					doc += metaData.getColumnName(i) + "||";
+				doc += metaData.getColumnName(i)+"\n";
+				
+				//Loop through the rows until rs.next returns false
+				do
+				{
+					for(i = 1; i < count; i++)
+						doc += rs.getString(i) + "||";
+					doc += rs.getString(count) + "\n";
+				} while(rs.next());
+				
+			}
+			 
 		}
 		catch(Exception e)
 		{ 
-			System.out.println("uh oh"); 
-			return "null";
+			System.out.println("uh oh\n"+e); 
+			return "Error";
 		}  
-		
 		return doc;
 	}
 	
-	//TODO: modify it to work for the new keyword system
-	public void add(String title, String content, String[] keywords)
+	public boolean add(String title, String content, String[] keywords)
 	{
+		//String sanitizedKeywords;  On second thought, parser.getTopTen always returns an array with 10 elems
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", username, password);
             Statement stmt = con.createStatement(); 
-            stmt.executeUpdate("insert into test.documents (title, content, keywords) values('"+title+"', '"+content+"', '"+keywords[0]+"')");
+            stmt.executeUpdate("insert into test.documents (title, content, keyword1, keyword2, keyword3, keyword4, keyword5, keyword6, keyword7, keyword8, keyword9, keyword10) "  
+            + "values('" + title + "', '" + content + "', '" + keywords[0] + "', '" + keywords[1] + "', '" + keywords[2] + "', '" + keywords[3] + "', '" + keywords[4] + "', '" 
+            + keywords[5] + "', '" + keywords[6] + "', '" + keywords[7] + "', '" + keywords[8] + "', '" + keywords[9] + "')");
         }
         catch(Exception e){
-            System.out.println(e); 
+            System.out.println("Failed to add to MySQL DB."); 
+            return false;
         }
+        
+        return true;
     }
 }
--- webClient.java
+++ webClient.java
@@ -2,27 +2,34 @@ import java.io.IOException;
 import java.net.*;
 import java.util.*;
 
-
-public class webClient 
+//TODO Implement a send method for uploading top ten words.
+//This will involve setting up different message types, defined by the header of the message.
+public class WebClient 
 {
 	Socket soc;
 	Scanner socScan;
 	
-	public webClient(int socketNum, InetAddress in)
+	//This is only for testing.  It will read a file instead of listening to a socket
+	public WebClient(String filname)
+	{
+		//TODO test this stuff with local files
+	}
+	
+	public WebClient(InetAddress in, int socketNum)
 	{
 		try {
 			soc = new Socket(in, socketNum);  
 			socScan = new Scanner(soc.getInputStream());
-			} 
+		} 
 		
 		catch (Exception e) {
 			System.out.println("Error: unable to connect to Socket.");  
 			socScan.close();
 			return;
-			}
+		}
 	}
 	
-	public webClient(int socketNum, String hostName)
+	public WebClient(String hostName, int socketNum)
 	{
 		try {
 			soc = new Socket(hostName, socketNum);  
@@ -31,36 +38,34 @@ public class webClient
 	
 		catch (Exception e) {
 			System.out.println("Error: unable to connect to Socket.");  
-			socScan.close();
+			//socScan.close();
 			return;
-			}
+		}
 	}
 	
 	//TODO think about infinite runtime
 	public void waitForInput() throws InterruptedException
 	{
-		while(true)
+		while(!socScan.hasNext())
 		{
-			while(!socScan.hasNext())
-			{
-				Thread.sleep(1000);
-			}
-			
-			String[] input = readInput();
+			Thread.sleep(1000);
+		}
 		
-			if(input[0] == null && input[1] == null)
-			{
-				System.out.println("Error: Unable to read input");
-			}
-			else 
-			{
-				Parser p = new Parser(new Scanner(input[1]));
-				String[] topTen = p.getTopTen();
-				
-				ParserDemo.insertMySQL(input[0], input[1], topTen);
-			}
+		String[] input = readInput();
+	
+		if(input[0] == null && input[1] == null)
+		{
+			System.out.println("Error: Unable to read input");
+		}
+		else 
+		{
+			Parser p = new Parser(new Scanner(input[1]));
+			String[] topTen = p.getTopTen();
+			
+			ParserDemo.insertMySQL(input[0], input[1], topTen);
 		}
 	}
+	
 	
 	public String[] readInput()
 	{
@@ -84,4 +89,9 @@ public class webClient
 		return ret;
 	}
 	
+	public void close()
+	{
+		socScan.close();
+		try { soc.close(); } catch (IOException e) { System.out.println("Error: cannot close web socket."); }
+	}
 }
