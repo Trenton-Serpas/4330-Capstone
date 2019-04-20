47e4bfa1482453e4d2bf480eb6011d47a01ae130
Andrew Core <andrew.core@computrols.com>
2019-04-20 18:15:04 -0500

    ParserDemo and webClient have been replaced by main and SocketServer. Ready to implement the 9 methods in client-server-message.txt

---
+++ main.java
@@ -0,0 +1,11 @@
+
+public class main 
+{
+
+	public static void main(String[] args) 
+	{
+		SocketServer ssoc = new SocketServer(61111);
+		ssoc.initWeb();
+		
+	}
+}
--- webClient.java
+++ webClient.java
@@ -7,17 +7,18 @@ import java.util.*;
 public class WebClient 
 {
 	Socket soc;
-	public static MySQLConnector msdbc;
 	Scanner socScan;
 	
-	public static void main(String[] args)
+	//This is only for testing.  It will read a file instead of listening to a socket
+	public WebClient(String filname)
 	{
-		//Currently main simply creates and closes a MySQL and web connection.  Below is code I used for testing.
-		msdbc = new MySQLConnector("Andrew", "cbasfish", "test");
-		
-		//Note: this only works if there's an active server on the port you specify
+		//TODO test this stuff with local files
+	}
+	
+	public WebClient(InetAddress in, int socketNum)
+	{
 		try {
-			soc = new Socket("127.0.0.1", 80);  
+			soc = new Socket(in, socketNum);  
 			socScan = new Scanner(soc.getInputStream());
 		} 
 		
@@ -26,60 +27,20 @@ public class WebClient
 			socScan.close();
 			return;
 		}
-		
-		/*//For testing this.retrieveTopTen.  Requires a database with entries.
-		String[] sa = retriveTopTen("TLOTR 1");
-		for(int i = 0; i < sa.length; i++)
-			System.out.println(sa[i]);
-		*/
-		
-		//For Testing mySqlConnector.retrieve.  
-		//String temp = msdbc.retrieve("Select * from documents;");
-		//System.out.println(temp);
-		
-		
-		/*//Add garbage data
-		String[] keywords = new String[10];
-		keywords[0] = "asdfasdfasd";
-		keywords[1] = "the";
-		keywords[2] = "blaasdfh";
-		keywords[3] = "a";
-		keywords[4] = "a";
-		keywords[5] = "a";
-		keywords[6] = "a";
-		keywords[7] = "o";
-		keywords[8] = "sd";
-		keywords[9] = "ksd";
-		if(msdbc.add("TLOTR 1", "This is the body of the text.", keywords))
-			System.out.println("It worked!!!");
-			*/		
-		
-		/*  //Read files directly
+	}
+	
+	public WebClient(String hostName, int socketNum)
+	{
 		try {
-			FileReader fr = new FileReader("C:\\Users\\Trent\\Desktop");
-			BufferedReader br = new BufferedReader(fr);
+			soc = new Socket(hostName, socketNum);  
+			socScan = new Scanner(soc.getInputStream());
+		} 
+	
+		catch (Exception e) {
+			System.out.println("Error: unable to connect to Socket.");  
+			//socScan.close();
+			return;
 		}
-		*/
-		
-		msdbc.close();
-		close();
-	}
-	
-	public static void insertMySQL(String title, String content, String[] keywords)
-	{
-		msdbc.add(title, content, keywords);
-	}
-	
-	public static String[] retriveTopTen(String title)
-	{  //Considered doing this in one statement, but it's messy.  If there are two works with the same title, this only returns the first
-		String temp = msdbc.retrieve("select keyword1, keyword2, keyword3, keyword4, keyword5, keyword6, keyword7, keyword8, keyword9, keyword10 from documents where title = \"" + title + "\";");
-		return temp.split("\n", -1)[1].split("\\0174{2}", -1); //remove column names, second split patter is 2 copies of octal value 174 aka |
-	}
-	
-	//This is only for testing.  It will read a file instead of listening to a socket
-	public WebClient(String filname)
-	{
-		//TODO test this stuff with local files
 	}
 	
 	//TODO think about infinite runtime
@@ -101,7 +62,7 @@ public class WebClient
 			Parser p = new Parser(new Scanner(input[1]));
 			String[] topTen = p.getTopTen();
 			
-			ParserDemo.insertMySQL(input[0], input[1], topTen);
+			//ParserDemo.insertMySQL(input[0], input[1], topTen);
 		}
 	}
 	
@@ -126,6 +87,17 @@ public class WebClient
 		}
 		
 		return ret;
+	}
+	
+	public static void insertMySQL(String title, String content, String[] keywords)
+	{
+		msdbc.add(title, content, keywords);
+	}
+	
+	public static String[] retriveTopTen(String title)
+	{  //Considered doing this in one statement, but it's messy.  If there are two works with the same title, this only returns the first
+		String temp = msdbc.retrieve("select keyword1, keyword2, keyword3, keyword4, keyword5, keyword6, keyword7, keyword8, keyword9, keyword10 from documents where title = \"" + title + "\";");
+		return temp.split("\n", -1)[1].split("\\0174{2}", -1); //remove column names, second split patter is 2 copies of octal value 174 aka |
 	}
 	
 	public void close()
