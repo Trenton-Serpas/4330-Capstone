ac78ed719afa4a18c6dc0e66bf36234930afb37b
Andrew Core <andrew.core@computrols.com>
2019-04-22 04:27:36 -0500

    init mySQL commands

--- Final-4330-Submission/src/MasterServlet.java
+++ Final-4330-Submission/src/MasterServlet.java
@@ -22,7 +22,7 @@ public class MasterServlet extends HttpServlet
 	public void init() throws ServletException 
 	{
         mc = new MySQLConnector("Andrew", "cbasfish", "test");
-		System.out.println(mc.retrieve("Select * from documents;"));
+		//System.out.println(mc.retrieve("Select * from documents;"));
 	}
 	
 	
@@ -39,6 +39,13 @@ public class MasterServlet extends HttpServlet
 		//String text = new String(Files.readAllBytes(Paths.get(fPath + "signin.html")), StandardCharsets.UTF_8);
 		//response.getWriter().write(text);
 		
+		//System.out.println(mc.retrieve("select title from documents;"));
+		//System.out.println(getTitles());
+		//System.out.println(getBody("The Hobbit"));
+		//System.out.println(getKeywords("The Hobbit"));
+		
+		if(type == null)
+			return;
 
 		switch(type)
 		{
@@ -108,7 +115,7 @@ public class MasterServlet extends HttpServlet
 				String html7 = populateHTML("../HTML/output.html");
 				
 				//fetch title and text
-				String titleAndFullbody = getTitleAndFullbody();
+				String titleAndFullbody = getBody("temp");
 				
 				//output text		
 				
@@ -156,7 +163,7 @@ public class MasterServlet extends HttpServlet
 				//populate string
 				String html12 = populateHTML("../HTML/documents.html");
 				
-				boolean uploaded = uploadDoc(request.getParameter("title"), request.getParameter("body"));
+				uploadDoc(request.getParameter("title"), request.getParameter("body"));
 				
 				response.getWriter().write(populateTitles(html12));
 				
@@ -165,7 +172,7 @@ public class MasterServlet extends HttpServlet
 				//populate string
 				String html13 = populateHTML("../HTML/documents.html");
 				
-				boolean deleted = deleteDoc(request.getParameter("title"));
+				deleteDoc(request.getParameter("title"));
 				
 				response.getWriter().write(populateTitles(html13));
 	
@@ -181,7 +188,7 @@ public class MasterServlet extends HttpServlet
 	private String populateTitles(String html)
 	{
 		String titlesHTML = "";
-		List<String> titles = getTitles();
+		List<String> titles = Arrays.asList(getTitles());
 		
 		for(int i = 0; i < titles.size(); i++)
 		{
@@ -211,8 +218,8 @@ public class MasterServlet extends HttpServlet
 	
 	private ArrayList<String> search(String key, ArrayList<String> validTitles)
 	{
-		ArrayList<String> titles = getTitles();
-		
+		List<String> titles = Arrays.asList(getTitles());//getTitles();
+		titles.add("asdfasdf");
 		for(int i = 0; i < titles.size(); i++)
 		{
 			String[] keywords = getKeywords(titles.get(i));
@@ -229,7 +236,11 @@ public class MasterServlet extends HttpServlet
 		
 		return validTitles;
 	}
-	
+	public void deleteDoc(String title)
+	{
+		mc.retrieve("delete from documents where title = " + title + ";");
+		//return initTitles.length - getTitles().length; 
+	}
 	public void uploadDoc(String title, String body)
 	{
 		String[] keywords = new String[10];
@@ -237,5 +248,26 @@ public class MasterServlet extends HttpServlet
 		keywords = p.getTopTen();
 		
 		mc.add(title, body, keywords);
+	}
+	
+	public String[] getKeywords(String title)
+	{
+		String temp = mc.retrieve("select keyword1, keyword2, keyword3, keyword4, keyword5, keyword6, keyword7, keyword8, keyword9, keyword10 from documents where title = \"" + title + "\";");
+		return temp.split("\n", -1)[1].split("\\0174{2}", -1); //remove column names, second split patter is 2 copies of octal value 174 aka |
+	}
+	
+	public String getTitles()
+	{
+		return mc.retrieve("select title from documents;");
+	}
+	
+	public String getBody(String title)
+	{
+		return mc.retrieve("Select content from documents where title = \"" + title + "\";");
+	}
+	
+	public boolean createUser(String email, String pass)
+	{
+		return true;
 	}
 }
