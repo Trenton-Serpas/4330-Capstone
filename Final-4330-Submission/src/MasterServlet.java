f170ce10f9b88c1b47116e8a28b8a75649eace73
Andrew Core <andrew.core@computrols.com>
2019-04-22 01:57:21 -0500

    Can't get mysql to work

--- Final-4330-Submission/src/MasterServlet.java
+++ Final-4330-Submission/src/MasterServlet.java
@@ -1,7 +1,8 @@
-import javax.mail.*;
-import javax.mail.internet.*;
+//import javax.mail.*;
+//import javax.mail.internet.*;
 import java.util.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,12 +15,25 @@ public class MasterServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 
-
+	int count;
 	MySQLConnector mc;
+	
 	public void init() throws ServletException 
 	{
 	      mc = new MySQLConnector("Andrew", "cbasfish", "test");
-	      
+	      count = 0;
+	      System.out.println(count);
+	      File f = new File("C:\\Java\\TestFile"+count+".txt");
+	      FileWriter fw;
+		try {
+			fw = new FileWriter(f);
+			fw.append("uh plz work lol" +count);
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("fail");
+		}
+	    System.out.println(mc.retrieve("select * from documents"));
 	}
 	
 	
@@ -33,10 +47,18 @@ public class MasterServlet extends HttpServlet
 		String type = request.getParameter("type"); // this can also be done using different servlets
 		Scanner sc; // we will scan the html docs in each case and edit them before printing it using response writer
 		
-
-		
 		if(type == null)
 			return;
+		
+		File f = new File("C:\\Java\\TestFile"+count+".txt");
+		if(f.createNewFile())
+		{
+			f.setWritable(true);
+			FileWriter fw = new FileWriter(f);
+			fw.write("uh plz work lol" +count);
+			fw.close();
+			return;
+		}
 		
 		switch(type)
 		{
@@ -45,10 +67,10 @@ public class MasterServlet extends HttpServlet
 			sc = new Scanner(new File("../HTML/documents.html"));
 			
 			//create user
-			boolean creationSuccess = createUser(request.getParameter("email"), request.getParameter("pass"));
+			//boolean creationSuccess = createUser(request.getParameter("email"), request.getParameter("pass"));
 			
 			//fetch titles
-			List<String> titlesC = getTitles();
+			//List<String> titlesC = getTitles();
 			
 			//update default titles
 			
@@ -58,14 +80,14 @@ public class MasterServlet extends HttpServlet
 		case "login"://sends to Documents
 			
 			//verify user
-			boolean loginSuccess = login(request.getParameter("email"), request.getParameter("pass"));
+			//boolean loginSuccess = login(request.getParameter("email"), request.getParameter("pass"));
 			
 			//fetch titles
-			List<String> titlesL = getTitles();
+			//List<String> titlesL = getTitles();
 			
 			//update default titles
 			
-			
+		/*	
 		case "email":
 			Properties properties = System.getProperties();
 			properties.setProperty("mail.smtp.host", "localhost");
@@ -88,7 +110,7 @@ public class MasterServlet extends HttpServlet
 			{
 				
 			}
-			
+			*/
 		case "titles":
 			
 		case "titleAndFullbody":
